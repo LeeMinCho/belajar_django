5a364b13fe5eb3a408f95b64c80096cd8f68557d
Eka Yunan <ekayunan98@gmail.com>
2022-04-15 21:14:57 +0700

    Membuat App

--- first_django/urls.py
+++ first_django/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from profil import views as profilViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('profil', profilViews.index, name='profil')
 ]
